9a10566ff2c72457511d1184c7715c0558f29022
RaviAnand Mohabir <moravrav@gmail.com>
2023-04-05 12:28:19 +0200

    chore: :wrench: add Prettier & ESLint & GraphQL Codegen

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import codegen from "vite-plugin-graphql-codegen";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    codegen({
+      config: {
+        schema: "http://localhost:5001/graphql",
+        documents: ["src/**/*.{ts,tsx}"],
+        generates: {
+          "./src/gql/": {
+            preset: "client",
+            plugins: [],
+          },
+        },
+      },
+    }),
+  ],
+});
