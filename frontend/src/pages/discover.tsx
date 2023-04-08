29854387d0902dd16ee47e2587da4823d0332c5a
RaviAnand Mohabir <moravrav@gmail.com>
2023-04-09 00:13:44 +0200

    feat: :sparkles: load movie suggestions on discovery page and display in carousel

--- frontend/src/pages/discover.tsx
+++ frontend/src/pages/discover.tsx
@@ -1,9 +1,23 @@
 import { useQuery } from "@apollo/client";
-import { Group, ScrollArea, Stack, Title } from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
+import {
+  Box,
+  Group,
+  Image,
+  Paper,
+  ScrollArea,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+import Autoplay from "embla-carousel-autoplay";
+import { useRef } from "react";
 import Poster from "../components/poster";
 import { graphql } from "../gql";
 
 const Discover = () => {
+  const autoplay = useRef(Autoplay({ delay: 2000 }));
+
   const { data } = useQuery(
     graphql(`
       query Discover {
@@ -21,12 +35,66 @@ const Discover = () => {
           posterPath
           backdropPath
         }
+        movieSuggestions {
+          id
+          tmdbId
+          title
+          tagline
+          posterPath
+          backdropPath
+        }
       }
     `),
   );
 
   return (
     <Stack spacing="md">
+      <Title color="white">Movies For You</Title>
+      <Carousel
+        withIndicators
+        height={500}
+        loop
+        plugins={[autoplay.current]}
+        onMouseEnter={autoplay.current.stop}
+        onMouseLeave={autoplay.current.reset}
+      >
+        {data?.movieSuggestions.map((suggestion) => (
+          <Carousel.Slide key={suggestion.id}>
+            <Paper>
+              <Box h={470} sx={{ overflow: "hidden", position: "relative" }}>
+                <Image
+                  src={`https://image.tmdb.org/t/p/original${suggestion.backdropPath}`}
+                  sx={{ objectPosition: "center", objectFit: "contain" }}
+                />
+                <Box
+                  sx={(theme) => ({
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    background: theme.fn.linearGradient(
+                      360,
+                      theme.fn.rgba(theme.colors.dark[6], 0.8),
+                      theme.fn.rgba(theme.colors.dark[6], 0.8),
+                      theme.fn.rgba(theme.colors.dark[6], 0),
+                    ),
+                  })}
+                >
+                  <Stack justify="end" h="100%" p="lg" spacing="xs">
+                    <Title order={3} color="white">
+                      {suggestion.title}
+                    </Title>
+                    <Text sx={(theme) => ({ color: theme.colors.gray[3] })}>
+                      {suggestion.tagline}
+                    </Text>
+                  </Stack>
+                </Box>
+              </Box>
+            </Paper>
+          </Carousel.Slide>
+        ))}
+      </Carousel>
       <Title color="white">Movies</Title>
       <ScrollArea styles={{ viewport: { overflowY: "hidden" } }}>
         <Group
