f671adff3a06d0d9e9e372e44c6374e0ea0b28fb
RaviAnand Mohabir <moravrav@gmail.com>
2023-04-14 20:35:48 +0200

    refactor: :recycle: move dsn to separate getter

--- server/pkg/db.go
+++ server/pkg/db.go
@@ -13,8 +13,8 @@ import (
 	zapgorm "moul.io/zapgorm2"
 )
 
-func NewDb(lc fx.Lifecycle, logger *zap.Logger) *gorm.DB {
-	dsn := fmt.Sprintf(
+func GetDsn() string {
+	return fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s",
 		viper.GetString("db.host"),
 		viper.GetString("db.user"),
@@ -23,10 +23,13 @@ func NewDb(lc fx.Lifecycle, logger *zap.Logger) *gorm.DB {
 		viper.GetInt("db.port"),
 		viper.GetString("db.sslmode"),
 	)
+}
+
+func NewDb(lc fx.Lifecycle, logger *zap.Logger) *gorm.DB {
 	log := zapgorm.New(logger)
 	log.SetAsDefault()
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+	db, err := gorm.Open(postgres.Open(GetDsn()), &gorm.Config{
 		Logger: log,
 	})
 	if err != nil {
