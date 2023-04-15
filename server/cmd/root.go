3eb4a8ed9369aa34b2edf1d451f4930764d07a72
RaviAnand Mohabir <moravrav@gmail.com>
2023-04-15 13:38:15 +0200

    chore: :speech_balloon: update jolt command usage

--- server/cmd/root.go
+++ server/cmd/root.go
@@ -12,7 +12,7 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "jolt-server",
+	Use:   "jolt",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
